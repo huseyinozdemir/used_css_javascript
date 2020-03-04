b8e19a3384d21e9ca89426b78839aa67b28072eb
Hüseyin Özdemir <husonet@pythongo.local>
2020-03-04 21:42:17 +0300

    Modified css process

--- app/css_process.py
+++ app/css_process.py
@@ -2,26 +2,92 @@ import re
 
 from functools import reduce
 
+RE_ELEMENT = '<([a-zA-Z]+)'
+RE_ELEMENT = re.compile(RE_ELEMENT, re.DOTALL)
 RE_CLASS = 'class="(.*?)"'
 RE_CLASS = re.compile(RE_CLASS, re.DOTALL)
 RE_ID = 'id="(.*?)"'
 RE_ID = re.compile(RE_ID, re.DOTALL)
+RE_CSS_FILE = 'href="(.*?\.css).*?"'
+RE_CSS_FILE = re.compile(RE_CSS_FILE, re.MULTILINE)
 
 
 class CssProcess:
+    REPLACE_DIC = {"\n": "",
+                   ", ": ",",
+                   ": ":":",
+                   "  ": ""}
+    _base = None
     _body = None
 
-    def __init__(self, body):
+    def __init__(self, base, body):
         self._body = body
+        self._base = base
+
+    def get_uniqe_list(self, val_list):
+        return reduce(lambda x, y: x+[y] if y not in x else x, val_list, [])
+
+    def get_element_list(self):
+        element_list = RE_ELEMENT.findall(self._body)
+        if not (element_list):
+            return
+        return self.get_uniqe_list(element_list)
 
     def get_class_list(self):
         class_list = RE_CLASS.findall(self._body)
         if not (class_list):
             return
-        return reduce(lambda x, y: x+[y] if y not in x else x, class_list, [])
+        return self.get_uniqe_list(class_list)
 
     def get_id_list(self):
         id_list = RE_ID.findall(self._body)
         if not (id_list):
             return
-        return reduce(lambda x, y: x+[y] if y not in x else x, id_list, [])
+        return self.get_uniqe_list(id_list)
+
+    def get_css_file_list(self):
+        file_list = RE_CSS_FILE.findall(self._body)
+        if not (file_list):
+            return
+        file_list = self.get_uniqe_list(file_list)
+        for index, fl in enumerate(file_list):
+            if not self._base in fl:
+                file_list[index] = '{}{}'.format(self._base, fl)
+                file_list[index] = file_list[index].replace('//', '/')
+        return file_list
+
+    def parse_css_id_on_body(self, material_list):
+        style = list();
+        for ml in material_list:
+            RE_CSS_PARSE = '({cls}{frmt})'.format(
+                cls='\#{}'.format(ml), frmt='.*?\{.*?\}'
+            )
+            RE_CSS_PARSE = re.compile(RE_CSS_PARSE, re.MULTILINE|re.DOTALL)
+            cs_list = RE_CSS_PARSE.findall(self._body)
+            if not (cs_list):
+                continue
+            for cs in cs_list:
+                tmp = cs
+                for i, j in self.REPLACE_DIC.items():
+                    tmp = tmp.replace(i, j);
+                style.append(tmp)
+
+        return style
+
+    def parse_css_class_on_body(self, material_list):
+        style = list();
+        for ml in material_list:
+            RE_CSS_PARSE = '({cls}{frmt})'.format(
+                cls='\.{}'.format(ml), frmt='.*?\{.*?\}'
+            )
+            RE_CSS_PARSE = re.compile(RE_CSS_PARSE, re.MULTILINE|re.DOTALL)
+            cs_list = RE_CSS_PARSE.findall(self._body)
+            if not (cs_list):
+                continue
+            for cs in cs_list:
+                tmp = cs
+                for i, j in self.REPLACE_DIC.items():
+                    tmp = tmp.replace(i, j);
+                style.append(tmp)
+
+        return style
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from app.css_process import CssProcess
 
+DOMAIN = 'https://www.eyzi.net/'
+
 
 if __name__ == '__main__':
     body = """
@@ -8,8 +10,68 @@ if __name__ == '__main__':
    <title>
     The Dormouse's story
    </title>
+   <link rel="canonical" href="https://www.pythontr.com" />
+   <link rel="stylesheet" href="/css/main.min.css" type="text/css" />
+   <link rel="preload" as="style" onload="this.rel='stylesheet'" href="https://www.eyzi.net/assets/css/font.awesome.min.css?display=swap&v=4" type="text/css">
+   <style>
+    div {
+        background:#fff;
+    }
+    p {
+        font-weight:10px;
+    }
+    .title{background-color:#fff;color:#000}
+    .sister {
+        margin: 0 auto;
+        display: block;
+        border: 2px solid #3bb8ea;
+        overflow: hidden;
+        border-radius: 14px;
+    }
+    .sister h1{
+        margin: 0 auto;
+    }
+    #link1 {
+        padding: 5px;
+        background-color: #e4e4e4;
+        margin: auto;
+        margin-top: -5px;
+        margin-bottom: 5px;
+        border-radius: 0 0 6px 6px;
+    }
+    #link1 #link2 {
+        padding: 5px;
+        background-color: #e4e4e4;
+        margin: auto;
+        margin-top: -5px;
+        margin-bottom: 5px;
+        border-radius: 0 0 6px 6px;
+    }
+    #link2 {position: absolute;top: 0;left: 0;color: blue;}
+    .story {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: #000;
+        background: rgba(0,0,0,0.7);
+        color: #f9d327;
+        transition: .5s ease;
+        opacity: 0;
+        font-size: 16px;
+        padding: 1px;
+        text-align: center;
+        border-bottom-left-radius: 15px;
+        border-bottom-right-radius: 15px;
+        border-left: 2px solid;
+        border-right: 2px solid;
+        border-bottom: 2px solid;
+    }
+   </style>
   </head>
   <body>
+  <div>
    <p class="title">
     <b>
      The Dormouse's story
@@ -33,9 +95,16 @@ if __name__ == '__main__':
    <p id="since" class="story">
     ...
    </p>
+   </div>
   </body>
  </html>
     """
-    cp = CssProcess(body)
-    print(cp.get_class_list())
-    print(cp.get_id_list())
+    cp = CssProcess(DOMAIN, body)
+    print(cp.get_element_list())
+    print(cp.get_css_file_list())
+    class_list = cp.get_class_list()
+    print(class_list)
+    print(cp.parse_css_class_on_body(class_list))
+    id_list = cp.get_id_list()
+    print(id_list)
+    print(cp.parse_css_id_on_body(id_list))
